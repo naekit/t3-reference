7d510ba69103d38da08145966aa6cbf22f972062
naekit <mykyta.onishchuck@code.berlin>
2023-03-26 18:38:37 -0400

    added loading state

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
+import { privateProcedure } from "../trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -15,7 +16,10 @@ const filterUserForClient = (user: User) => {
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.prisma.post.findMany({ take: 100 });
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      orderBy: { createdAt: "desc" },
+    });
 
     const users = (
       await clerkClient.users.getUserList({
@@ -23,8 +27,6 @@ export const postsRouter = createTRPCRouter({
         limit: 100,
       })
     ).map(filterUserForClient);
-
-    console.log(users);
 
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
@@ -45,4 +47,15 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+  create: privateProcedure
+    .input(z.object({ content: z.string().emoji().min(1).max(280) }))
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const post = await ctx.prisma.post.create({
+        data: { authorId, content: input.content },
+      });
+
+      return post;
+    }),
 });
